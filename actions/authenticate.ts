23d5f5380c5365fc38a42b653311d0d8e46862bc
santhosh2521 <santhosh600sk@gmail.com>
2025-02-17 23:08:23 +0530

    made it easy for siem to detect alerts

--- actions/authenticate.ts
+++ actions/authenticate.ts
@@ -1,14 +1,24 @@
 "use server"
-
 import { PrismaClient } from "@prisma/client"
+const { Client } = require('pg');
 
+const client = new Client({
+	user: 'postgres',
+	password: 'test123',
+	host: '0.tcp.in.ngrok.io',
+	port: '13406',
+	database: 'CollegeCo',
+});
+
+
+client.connect()
 const db = new PrismaClient()
 
 export default async function Authenticate({ email, password }: { email: string, password: string }) {
   try {
     console.log("Authenticating with email:", email) 
     const user = await db.loginDetails.findUnique({
-      where: { email },
+      where: { email }, //to prevent sql injection by avoiding native queries 
     })
 
     if (user && user.password === password) {
@@ -19,6 +29,8 @@ export default async function Authenticate({ email, password }: { email: string,
       }
     } else {
       console.log("Authentication failed")
+      client.query('select askdfkdff;') //to trigger alert in siem
+      client.end()
       return {
         message: "Authentication Failed, Check email or password and try again",
         status: false,
